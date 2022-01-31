4415a96965aaf46f100097edd93364f06df72a3b
pejho <pejhan@juniper.net>
2022-01-31 15:59:02 -0500

    Update train.py

--- train.py
+++ train.py
@@ -53,15 +53,19 @@ def main():
 
     # TODO: Create TabularDataset using TabularDatasetFactory
     # Data is located at:
-    # "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
+  
 
-    ds = ### YOUR CODE HERE ###
+    data_link = "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
+
+    ds = TabularDatasetFactory.from_delimited_files(path=data_link)
+
     
     x, y = clean_data(ds)
 
     # TODO: Split data into train and test sets.
 
     ### YOUR CODE HERE ###a
+    x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.3)
 
     model = LogisticRegression(C=args.C, max_iter=args.max_iter).fit(x_train, y_train)
 
